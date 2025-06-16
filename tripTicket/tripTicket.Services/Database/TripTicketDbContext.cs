0110a27796d3fe7b23fdfefc5ba7e88b4fc94e70
Faris Dizdarevic <faris.dizdarevic@edu.fit.ba>
2025-06-16 23:36:11 +0200

    Comment onconfigure

--- tripTicket/tripTicket.Services/Database/TripTicketDbContext.cs
+++ tripTicket/tripTicket.Services/Database/TripTicketDbContext.cs
@@ -37,9 +37,9 @@ public partial class TripTicketDbContext : DbContext
     public virtual DbSet<Role> Roles { get; set; }
     public virtual DbSet<UserRole> UserRoles { get; set; }
 
-    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("Data Source=localhost, 1433;Initial Catalog=TripTicketDB; user=sa; Password=QWEasd123!; TrustServerCertificate=True");
+//    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+//#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
+//        => optionsBuilder.UseSqlServer("Data Source=localhost, 1433;Initial Catalog=TripTicketDB; user=sa; Password=QWEasd123!; TrustServerCertificate=True");
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
